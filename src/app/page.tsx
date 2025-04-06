76c99e92774b7ba95d1a0c98010820da4ccb3377
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-06 19:01:06 +0300

    Add ProductsSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import NavigationMenu from '../components/NavigationMenu';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
+import ProductsSection from '../components/ProductsSection';
 
 export default function HomePage() {
   return (
@@ -13,6 +14,7 @@ export default function HomePage() {
         <NavigationMenu />
     <HeroSection />
     <AboutSection />
+    <ProductsSection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
